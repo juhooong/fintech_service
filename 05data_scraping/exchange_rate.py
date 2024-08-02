ac40d7d89c52e2483db6ede30cc5620e69ca0312
juhooong <juhooong@gmail.com>
2024-08-02 17:37:46 +0900

    2024-08-02: 하나은행 환율 자동저장하기, 네이버 핀테크 뉴스 DB자동저장하기

--- 05data_scraping/exchange_rate.py
+++ 05data_scraping/exchange_rate.py
@@ -32,15 +32,18 @@ driver.set_window_size(1920, 1080) #웹브라우저 해상도 조절
 driver.get("https://www.kebhana.com/cms/rate/index.do?contentUrl=/cms/rate/wpfxd651_01i.do#//HanaBank")
 
 wait = WebDriverWait(driver, 10) # 웹 요소가 나타날 때까지 최대 10초 기다림.
+time.sleep(5)
 
 # 날짜 입력창
 search_box = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#tmpInqStrDt")))
 search_box.clear()
 search_box.send_keys(today+Keys.ENTER)
+time.sleep(2)
 
 # 조회 버튼
 search_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#HANA_CONTENTS_DIV > div.btnBoxCenter > a")))
 search_button.click()
+time.sleep(5)
 
 # 환율 정보 
 exchange_rate_table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#searchContentDiv > div.printdiv > table")))
@@ -53,3 +56,5 @@ df.columns = cols # cols에 정의한 컬럼명으로 변경하기
 df = df[[*sorted_cols]] # date 컬럼을 가장 앞으로 보내기
 # DB에 저장
 dbio.exi_to_db("exchange_rate", today, df)
+driver.close()
+
---
+++ 05data_scraping/naver_api_info.py
@@ -0,0 +1,9 @@
+client_id = "jUA_F5ponFWfrb7IxITa" # 네이버 api에 접속 가능한 id 
+client_secret = "DUFTGcRdSQ"       # 네이버 api에 접속 가능한 pw 
+
+db="mysql"
+dbtype="pymysql"
+id="root"
+pw="1234"
+host="127.0.0.1:3306"
+news_database="fintech_news"
